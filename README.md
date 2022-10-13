dc7b33799fc717d4e665d09caf67e91fd04c0a74
Lailson Sobral <lailsonlm@hotmail.com>
2022-10-12 21:53:17 -0300

    finalize application frontend and create readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ignite Lab - Design System
+
+![Ignite Lab - Design System](./lab-ds.png)
+
+> Projeto desenvolvido no **Ignite Lab 3** da Rocketseat. A aplicação foi desenvolvida por completa, desde o projeto dentro do Figma, criando todo o Design System até a construção em React.JS e finalizando com a documentação dos componentes utilizando o Storybook.
+
+<br />
+
+## Tecnologias e ferramentas utilizadas
+- React.JS (Vite)
+- TypeScript
+- Radix UI
+- Tailwind CSS
+- Clsx
+- Storybook
+- Addon A11y
+- Github Actions
+
+Para acessar a documentação no Storybook [Clique aqui!](https://lailsonlm.github.io/ignite-lab-design-system/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,71 @@
+import { Envelope, Lock } from "phosphor-react";
 import { Button } from "./components/Button";
+import { Checkbox } from "./components/Checkbox";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+import { Text } from "./components/Text";
+import { TextInput } from "./components/TextInput";
 
 export function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900">
-      <form className="flex flex-col w-full max-w-[400px]">
-        <div>
-          <label htmlFor="">
-          </label>
-        </div>
-        <Button>
-          Teste
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
+
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text 
+          size="lg"
+          className="text-gray-400 mt-1"
+        >
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
+      <form className="flex flex-col items-stretch w-full max-w-[400px] mt-10 gap-4">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+
+            <TextInput.Input type="email" id="email" placeholder="Digite seu e-mail" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text>Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+
+            <TextInput.Input type="password" id="password" placeholder="********" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200">Lembrar de mim por 30 dias</Text>
+        </label>
+
+        <Button type="submit" className="mt-4">
+          Entrar na plataforma
         </Button>
       </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text asChild size="sm">
+          <a href="" className="text-gray-400 underline hover:text-gray-200">Esqueceu sua senha?</a>
+        </Text>
+        <Text asChild size="sm">
+          <a href="" className="text-gray-400 underline hover:text-gray-200">Não possui conta? Crie uma agora!</a>
+        </Text>
+        
+      </footer>
     </div>
   )
 }
